b490b823c68fafaa1768e8d4739cc8438f5dfcea
铂屑 <80164656+boxie123@users.noreply.github.com>
2022-09-03 14:02:04 +0800

    Initial commit

---
+++ main.py
@@ -0,0 +1,55 @@
+import re
+
+from pandas import DataFrame
+
+
+def txt_to_dict() -> dict[str]:
+    path = input("请将txt文件拖到此处并回车：")
+    print('正在分析txt文件……')
+    try:
+        with open(path, "r", encoding="utf-8") as f:
+            data_list = f.readlines()
+    except FileNotFoundError as e:
+        print(f"此文件不存在：\n{e}")
+        input()
+        exit()
+
+    data_dict = {}
+    for i in range(len(data_list)):
+        data = data_list[i]
+        split_list = data.split()
+        if len(split_list) < 5:
+            print(f"第{i+1}行内容错误：{data}")
+            continue
+        name_and_uid = split_list[2]
+        gift = split_list[4]
+        pattern = re.compile(r"(.+)\((\d+?)\)$")
+        match = re.search(pattern, name_and_uid)
+        if match:
+            name = match.group(1)
+            uid = match.group(2)
+        else:
+            print(f"匹配失败：{data}")
+            continue
+        if uid not in data_dict:
+            data_dict[uid] = {
+                "name": name,
+                gift: 1,
+            }
+        elif gift not in data_dict[uid]:
+            data_dict[uid][gift] = 1
+        else:
+            data_dict[uid][gift] += 1
+
+    return data_dict
+
+
+def dict_to_xlsx(data_dict: dict):
+    print('正在写入xlsx文件……')
+    dataframe = DataFrame(data_dict).T
+    dataframe.to_excel("data.xlsx", "data", na_rep=0)
+
+
+if __name__ == "__main__":
+    dict_to_xlsx(txt_to_dict())
+    input("完成！请查看与本程序同文件夹的‘data.xlsx’。\n回车退出程序")
